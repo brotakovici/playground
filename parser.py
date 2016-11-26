8914891f1b4152336aecf989abed88810cc56a8d
brotakovici <brotakovici@gmail.com>
2016-11-26 14:39:44 +0000

    added wordcount and stuff for statistics

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 import nltk
+from nltk.tree import Tree
+from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier
+from licenseHandler import LicenseHandler
 #import spacy
 
 #en_nlp = spacy.load('en')
@@ -14,11 +17,12 @@ lines = open('data.txt', 'r').readlines()
 
 grammar = r"""
   NP: {<DT|PP\$>?<JJ>*<NN>}   # chunk determiner/possessive, adjectives and noun
-      {<NNP>+},                # chunk sequences of proper nouns
+      {<NNP>+},               # chunk sequences of proper nouns
   VP: {<V.*><NP>},
   DEF: {<VBZ><NP>}
 """
 
+'''
 train = [("A vehicle is a human-made machine that provides transport.", True),
         ("I like pie.", False),
         ("The cube is a shape.", True),
@@ -29,7 +33,7 @@ train = [("A vehicle is a human-made machine that provides transport.", True),
 
 model = NaiveBayesClassifier(train)
 definition = "DEF: {<>}"
-
+'''
 def filt(n):
     return n.label()=='NP'
 
@@ -38,17 +42,45 @@ def getConcepts(tree):
     for subtree in tree.subtrees(filter = filt):
         print subtree
 
+parser = nltk.RegexpParser(grammar)
+
+'''
+handler = LicenseHandler('licenses/Apache-2.0.txt')
+definitions = handler.getDefinitions()
+
+parser = nltk.RegexpParser(grammar)
+
+for definition in definitions:
+    tokens = nltk.word_tokenize(definition)
+    tagged = nltk.pos_tag(tokens)
+    print definition
+    #print tagged
+    tree = parser.parse(tagged)
+    getConcepts(tree)
+    tree.draw() #Draws the tree
+    t = TextBlob(definition)
+    blobTree = t.parse()
+    print blobTree
+    print t.noun_phrases
+    #blob2nltk = Tree(blobTree)
+'''
 
 for line in lines:
     tokens = nltk.word_tokenize(line)
     tagged = nltk.pos_tag(tokens)
-    #print line
+    print line
     #print tagged
-    parser = nltk.RegexpParser(grammar)
     tree = parser.parse(tagged)
     getConcepts(tree)
     tree.draw() #Draws the tree
+    t = TextBlob(line)
+    blobTree = t.parse()
+    print blobTree
+    print t.noun_phrases
+    #blob2nltk = Tree(blobTree)
 
+'''
 print model.classify("The car is a vehicle.")
 print model.classify("I don't like pie.")
 print model.classify("The bed is a piece of furniture.")
+'''
