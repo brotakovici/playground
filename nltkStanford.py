ff20d6a6d2c90510b18671206e672951b1060b5f
brotakovici <brotakovici@gmail.com>
2017-02-20 13:24:00 +0000

    Adding and setting up nltk-stanford parser

--- nltkStanford.py
+++ nltkStanford.py
@@ -2,10 +2,10 @@ import nltk
 from nltk.parse.stanford import StanfordParser
 
 path_to_jar = "PLACEHOLDER"
-path_to_models_jar = "PLACEHOLDER"
+path_to_models_jar = "./lib/stanford-english-corenlp-2016-10-31-models.jar"
 
 dependency_parser = StanfordParser(path_to_jar = path_to_jar, path_to_models_jar = path_to_models_jar)
 
 result = dependency_parser.raw_parse('I shot an elephant in my sleep')
 dep = result.next()
-list(dep.triples())
+list(dep.triples())l
