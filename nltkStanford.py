7c14561b33029be09b39384bd9831d9217a2e880
brotakovici <brotakovici@gmail.com>
2017-02-20 14:02:42 +0000

    Fixed classpaths

--- nltkStanford.py
+++ nltkStanford.py
@@ -1,11 +1,11 @@
 import nltk
 from nltk.parse.stanford import StanfordParser
 
-path_to_jar = "PLACEHOLDER"
-path_to_models_jar = "./lib/stanford-english-corenlp-2016-10-31-models.jar"
+path_to_jar = "/Users/DMNDPL1/Projects/Code/3rd Year/playground/lib/stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0.jar"
+path_to_models_jar = "/Users/DMNDPL1/Projects/Code/3rd Year/playground/lib/stanford-english-corenlp-2016-10-31-models.jar"
 
 dependency_parser = StanfordParser(path_to_jar = path_to_jar, path_to_models_jar = path_to_models_jar)
 
 result = dependency_parser.raw_parse('I shot an elephant in my sleep')
 dep = result.next()
-list(dep.triples())l
+list(dep.triples())
