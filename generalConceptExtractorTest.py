54324657780fd4fde28b7a66bc8453c177191004
brotakovici <brotakovici@gmail.com>
2017-02-23 15:48:09 +0000

    Wrote test script for general concept extraction, need to think about the data structures, and make the output nicer, might move the data massage stuff to license handler

--- generalConceptExtractorTest.py
+++ generalConceptExtractorTest.py
@@ -1,11 +1,20 @@
 from conceptExtractor import ConceptExtractor
 import sys
+from nltk.tokenize import sent_tokenize
 
 filename = sys.argv[1]
 
 f = open(filename, 'r')
 
+data = f.read()
+
+# Data massaging might need to be moved to the licenseHandler class
+sentences = sent_tokenize(data)
+
+sentences = list(map(lambda sentence: sentence.replace("\n", " "), sentences))
+print sentences
+
 conceptExtractor = ConceptExtractor()
 
-for line in f:
+for sentence in sentences:
     print conceptExtractor.extractGeneralConcepts(sentence)
