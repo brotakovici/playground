b99c43d25a6af4ca8b188161b0102deccb2b245c
brotakovici <brotakovici@gmail.com>
2017-03-13 17:22:36 +0000

    Added test for stemmer vs lemma matching benchmark

--- generalConceptExtractorTest.py
+++ generalConceptExtractorTest.py
@@ -11,11 +11,43 @@ data = f.read()
 # Data massaging might need to be moved to the licenseHandler class
 sentences = sent_tokenize(data)
 
+doc = open(sys.argv[2], 'r')
+
+docData = doc.read()
+
+docSents = sent_tokenize(docData)
+docSents = map(lambda sent: sent.decode('unicode-escape'), docSents)
+
 sentences = list(map(lambda sentence: sentence.replace("\n", " "), sentences))
+docSents = list(map(lambda sentence: sentence.replace("\n", " "), docSents))
 
 conceptExtractor = ConceptExtractor()
 
-for sentence in sentences:
-    for concepts in conceptExtractor.extractGeneralConcepts(sentence):
-        print concepts
-        print '------------------------------------------------------'
+
+concepts = []
+
+for definition in sentences:
+    concepts.append(conceptExtractor.extractDefinedConcept(definition))
+
+concepts = filter(lambda x: x is not None, concepts)
+
+stemAnalysis = []
+lemmaAnalysis = []
+
+for concept in concepts:
+    stemAnalysis.append((concept, conceptExtractor.matchConcept(concept, docSents, True)))
+    #print stemAnalysis
+    lemmaAnalysis.append((concept, conceptExtractor.matchConcept(concept, docSents, False)))
+
+for i in range(len(stemAnalysis)):
+    if stemAnalysis[i][1] != lemmaAnalysis[i][1]:
+        print "-------------------------------------"
+        print "For concept :"
+        concept = stemAnalysis[i][0]
+        print concept[0]
+        print "Stem matching extracted:"
+        print len(stemAnalysis[i][1]), " occurences"
+        print stemAnalysis[i][1]
+        print "Lemma matching extracted:"
+        print len(lemmaAnalysis[i][1]), " occurences"
+        print lemmaAnalysis[i][1]
